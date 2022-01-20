b548e21fdb7bf82c1595df76320727e291214e10
Malakhov-cmd <mgs-2000@yandex.ru>
2022-01-20 14:48:27 +0400

    Homework to lecture 7 init

--- src/main/java/com/sbrf/reboot/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sbrf.reboot.repository;
-
-import java.io.IOException;
-import java.util.Set;
-
-public interface AccountRepository {
-    Set<Long> getAllAccountsByClientId(long clientId) throws IOException;
-    boolean changeAccountByClientIdAndOldAccountNumber(long clientId, long oldAccountNumber, long newAccountNumber) throws IOException;
-}
