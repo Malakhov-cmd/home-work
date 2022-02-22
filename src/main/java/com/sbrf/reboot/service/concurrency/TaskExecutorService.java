5f666b19aeba556c16fdfd8ed8778f0bae5787b3
Malakhov-cmd <mgs-2000@yandex.ru>
2022-02-22 12:51:26 +0400

    Homework to lecture 13 with additional tests

--- src/main/java/com/sbrf/reboot/service/concurrency/TaskExecutorService.java
+++ src/main/java/com/sbrf/reboot/service/concurrency/TaskExecutorService.java
@@ -2,6 +2,8 @@ package com.sbrf.reboot.service.concurrency;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class TaskExecutorService {
 
@@ -10,16 +12,24 @@ public class TaskExecutorService {
     private final ExecutorService service;
 
     public TaskExecutorService(int numberOfThreads) {
-        this.service = Executors.newFixedThreadPool(numberOfThreads);
         this.numberOfThreads = numberOfThreads;
+        this.service = Executors.newFixedThreadPool(recommendedThreadCount());
     }
 
     public void execute(Task task) {
-        service.execute(task);
+        IntStream
+                .range(0, numberOfThreads)
+                .mapToObj(i -> task)
+                .forEach(service::execute);
     }
 
     public void shutdown() {
         service.shutdown();
     }
 
+    public int recommendedThreadCount()
+    {
+        int cores = Runtime.getRuntime().availableProcessors() - 1;
+        return Math.min(cores, this.numberOfThreads);
+    }
 }
--- src/test/java/com/sbrf/reboot/concurrency/TaskExecutorServiceTest.java
+++ src/test/java/com/sbrf/reboot/concurrency/TaskExecutorServiceTest.java
@@ -35,4 +35,36 @@ public class TaskExecutorServiceTest {
 
         taskExecutorService.shutdown();
     }
+
+    @Test
+    public void successRunMultithreadingOnRecommendedCountOfThread() throws InterruptedException {
+
+        Task task = Mockito.mock(Task.class);
+        CountDownLatch latch = new CountDownLatch(36);
+
+        TaskExecutorService taskExecutorService = new TaskExecutorService(36);
+
+        doAnswer((e -> {
+            latch.countDown();
+            return null;
+        })).when(task).run();
+
+        taskExecutorService.execute(task);
+
+        latch.await();
+
+        assertEquals(0, latch.getCount());
+        verify(task, times(36)).run();
+        /*
+            Запускаемые потоки не разделяют ресурсы и не выполняют вычисления над
+            большим количеством данных, поэтому запускать количество потоков большее
+            значения - (количество ядер - 1) не имеет смысла.
+            Функционал получения количества ядер исполняемой машины приведен в методе
+            taskExecutorService.recommendedThreadCount().
+            Моя машина имеет 8 ядер.
+         */
+        assertEquals(7 , taskExecutorService.recommendedThreadCount());
+
+        taskExecutorService.shutdown();
+    }
 }
